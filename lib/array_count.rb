0bf41f2629c0f444a58648cd61ddb1a5396ed04b
Sanam Malik <sanam.malik04@gmail.com>
2020-07-08 19:46:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,6 @@ def count_strings(array)
   return array.count do |element| 
     element.string?
 end
-  return 
   # Return the total number of strings in the provided array using the count enumerable
 end
 
